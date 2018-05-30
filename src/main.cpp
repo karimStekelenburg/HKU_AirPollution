c263ed63ac35f8130ff613177cca0b7ac49d7156
Karim <karim@liberta.nl>
2018-05-30 12:17:41 +0200

    added single NUMA test scrips (reads position from serial)

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,19 @@
 #include <Numa17Control.h>
 
 Numa17Control numa(12, 13);
-
+int inputPos;
 void setup(){
-
+  Serial.begin(9600);
 }
 
 void loop(){
-  numa.setPosition(100);
+  inputPos = 200;
+  if(Serial.available() > 0){
+      inputPos = Serial.parseInt();
+  }
+
+  if(inputPos != 200){
+    numa.setPosition(inputPos);
+  }
   numa.run();
 }
